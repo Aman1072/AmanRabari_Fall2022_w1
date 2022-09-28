fd33e2aca7ac8bf3910265068524995249cdd716
Rabari <a_rabari@ATS-HR420-DM14.local>
2022-09-28 17:10:32 -0400

    filework

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 // imports go at the top of the JS file
-import { profs } from "./modules/data.js";
+import { getData } from "./modules/dataMiner.js";
 // this is an IIfe (immediately invoked function expression)
 // this is great for encapsulation code/ making 
 
 (() => {
     console.log('fired');
 
+
     //get a referance to the elemnt on page 
     let theTemplate = document.querySelector("#user-template").content,
         theTeam = document.querySelector('.team-section');
 
     
-    function changeCopy() {
+    function changeCopy(profs) {
         //parse the top-level profs from the profs object (the prof names)
         let theProfs = Object.keys(profs);
 
@@ -22,7 +23,7 @@ import { profs } from "./modules/data.js";
             let panel = theTemplate.cloneNode(true),
                 containers = panel.firstElementChild.children;
 
-            containers[0].querySelector('img').src = `images/${profs[prof].avatar}`;
+            containers[0].querySelector('img').src = `images/${profs[prof].biopic}`;
 
             containers[1].textContent = profs[prof].name;
             containers[2].textContent = profs[prof].role;
@@ -32,5 +33,6 @@ import { profs } from "./modules/data.js";
 
     }
     
-    changeCopy();
+    // retrieve our prof data, and then build out the content
+    getData(changeCopy);
 })();
---
+++ js/modules/dataMiner.js
@@ -0,0 +1,20 @@
+function getData(callback) {
+    console.log('fired from the data mine module');
+
+    //fetch is a JS API that runs AJAX requests
+    //and gets data from a resource
+    fetch(`./data.json`) // pass in the path to the data source
+        .then(res => res.json()) // convert JSON to plain JS object 
+        // the res is the data that we are retrieving from the resource
+        .then(data => { // data is converted JSON object -> now it is just data (JS object)
+            console.log(data);
+
+            //run the call back when we get all of our data back and ready to go 
+            //this gets passed in by referance when we invoke the dataMiner in main.js
+            callback(data);
+        })
+    .catch(error => console.error(error));
+    // catch any error that might happens and report to them    
+}
+
+export { getData }
